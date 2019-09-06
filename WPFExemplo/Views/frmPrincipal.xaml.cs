f362772c1b40b1b4c7e849f3cece831f6d3eebbd
Aron Rissato <aron_rissato@hotmail.com>
2019-09-06 20:11:58 -0300

    Mensagem na tela

---
+++ WPFExemplo/Views/frmPrincipal.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFExemplo.Views
+{
+    /// <summary>
+    /// Lógica interna para frmPrincipal.xaml
+    /// </summary>
+    public partial class frmPrincipal : Window
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMensagem_Click(object sender, RoutedEventArgs e)
+        {
+            string mensagem = txtMensagem.Text;
+            MessageBox.Show(mensagem, "WPF Exemplo");
+
+            if (string.IsNullOrEmpty(txtMensagem.Text))
+            {
+                MessageBox.Show(mensagem, "WPF Exemplo");
+            }
+
+
+            //if (txtMensagem.Text.Equals(""))
+            //{
+            //    MessageBox.Show("Campo vazio!");
+            //}
+            //else
+            //{
+            //    MessageBox.Show(mensagem, "WPF Exemplo");
+            //}
+
+        }
+
+        private void TxtMensagem_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
